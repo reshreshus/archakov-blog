fb277a901332b011c99a28807eb64d2f761a213a
reshreshus <reshreshus@gmail.com>
2019-10-30 17:58:58 +0300

    add CRUD operations

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,14 @@ var _Post = require('./models/Post');
 
 var _Post2 = _interopRequireDefault(_Post);
 
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // const mongoose = require('mongoose');
@@ -15,11 +23,56 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 _mongoose2.default.connect('mongodb://localhost:27017/blog');
 
-var post = new _Post2.default({
-    title: "Первая запись",
-    text: "Hello Post!"
+var app = (0, _express2.default)();
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use(_bodyParser2.default.json());
+
+app.post('/posts', function (req, res) {
+    var data = req.body;
+    console.log(data);
+    var post = new _Post2.default({
+        title: data.title,
+        text: data.text
+    });
+
+    post.save().then(function () {
+        res.send({ status: 'ok' });
+    });
 });
 
-post.save().then(function () {
-    console.log('OK!');
+app.get('/posts', function (req, res) {
+    _Post2.default.find().then(function (err, posts) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+
+        res.json(posts);
+    });
+});
+
+app.delete('/posts/:id', function (req, res) {
+    _Post2.default.remove({
+        _id: req.params.id
+    }).then(function (post) {
+        if (post) {
+            res.send({ status: 'deleted' });
+        } else {
+            res.send({ status: 'error' });
+        }
+    });
+});
+
+app.put('/posts/:id', function (req, res) {
+    _Post2.default.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json({ status: 'updated' });
+        }
+    });
+});
+
+app.listen(3333, function () {
+    console.log('SERVER STARTED!');
 });
--- src/index.js
+++ src/index.js
@@ -2,16 +2,63 @@
 // mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 import mongoose from 'mongoose';
-
 import Post from './models/Post';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 mongoose.connect('mongodb://localhost:27017/blog');
 
-const post = new Post({
-    title: "Первая запись",
-    text: "Hello Post!"
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.post('/posts', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const post = new Post({
+        title: data.title,
+        text: data.text
+    });
+    
+    post.save().then(() => {
+        res.send({ status: 'ok'});
+    })
 });
 
-post.save().then(() => {
-    console.log('OK!');
+app.get('/posts', (req, res) => {
+    Post.find().then( (err, posts) => {
+        if (err) {
+            res.send(err);
+            return;
+        }
+
+        res.json(posts);
+    });
+});
+
+
+app.delete('/posts/:id', (req, res) => {
+    Post.remove({
+        _id: req.params.id
+    }).then(post => {
+        if (post) {
+            res.send({status: 'deleted'});
+        } else {
+            res.send({status: 'error'})
+        }
+    })
+});
+
+app.put('/posts/:id', (req, res) => {
+    Post.findByIdAndUpdate(req.params.id, {$set: req.body}, (err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json({status: 'updated'});
+        }
+    });
+})
+
+app.listen(3333, () => {
+    console.log('SERVER STARTED!');
 })
